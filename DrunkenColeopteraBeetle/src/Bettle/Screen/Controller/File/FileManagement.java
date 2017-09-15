57d240270290d2dc6410bc78d1553e78bfab5300
Jeongsam <foryou8033j@gmail.com>
2017-09-16 00:12:09 +0900

    파일 출력 기능 추가 준비

---
+++ DrunkenColeopteraBeetle/src/Bettle/Screen/Controller/File/FileManagement.java
@@ -0,0 +1,16 @@
+package Bettle.Screen.Controller.File;
+
+/**
+ * 저장된 결과 데이터를 파일로의 출력을 지원한다.
+ * @author Jeongsam
+ *
+ */
+public class FileManagement {
+
+	
+	public void generateFile(){
+		
+	}
+	
+	
+}
