6392370450f40e3888f2c5dc75a840485939d4d7
Jeongsam <foryou8033j@gmail.com>
2017-09-15 23:14:39 +0900

    주석 수정

--- DrunkenColeopteraBeetle/src/Bettle/model/data/ResultData.java
+++ DrunkenColeopteraBeetle/src/Bettle/model/data/ResultData.java
@@ -3,7 +3,7 @@ package Bettle.model.data;
 
 
 /**
- *    Ŭ
+ * 결과 데이터 관리 클레스
  * @author Jeongsam
  *
  */
@@ -17,6 +17,15 @@ public class ResultData{
 		model = new ResultDataModel[100];
 	}
 
+	/**
+	 * 데이터를 저장한다.
+	 * 
+	 * @param width 가로
+	 * @param height 세로
+	 * @param time 시간
+	 * @param count 딱정벌레 갯수
+	 * @param delay 사용한 딜레이 시간
+	 */
 	public void saveData(int width, int height, String time, int count, int delay){
 		
 		model[dataCount] = new ResultDataModel(width, height, time, count, delay);
@@ -25,12 +34,27 @@ public class ResultData{
 		
 	}
 	
+	/**
+	 * 데이터 객체를 반환한다.
+	 * @return {@link ResultDataModel}
+	 */
 	public ResultDataModel[] getData(){
 		return model;
 	}
 	
+	/**
+	 * 데이터 갯수를 반환한다.
+	 * @return
+	 */
 	public int dataCount(){
 		return model.length;
+	}
+	
+	/**
+	 * 데이터 초기화
+	 */
+	public void resetData(){
+		model = new ResultDataModel[100];
 	}
 	
 	
