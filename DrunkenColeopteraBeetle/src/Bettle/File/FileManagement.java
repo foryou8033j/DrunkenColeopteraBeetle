04310a6a24925e2834449ac2bc92cb4a664ab0cb
Jeongsam <foryou8033j@gmail.com>
2017-09-16 23:22:42 +0900

    파일 저장 기능 구현

--- DrunkenColeopteraBeetle/src/Bettle/File/FileManagement.java
+++ DrunkenColeopteraBeetle/src/Bettle/File/FileManagement.java
@@ -1,4 +1,4 @@
-package Bettle.Screen.Controller.File;
+package Bettle.File;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 
+import Bettle.model.data.ResultData;
+import Bettle.model.data.ResultDataModel;
+
 /**
  * 저장된 결과 데이터를 파일로의 출력을 지원한다.
  * @author Jeongsam
@@ -16,22 +19,65 @@ import java.io.UnsupportedEncodingException;
 public class FileManagement {
 
 	
-	public void generateFile(File saveFile){
+	public void generateFile(File saveFile, ResultData resultDatas){
 		
-		String csvFilePath = saveFile.getAbsolutePath();
+		//사용자가 확장자를 붙였을 때 제거한다.
+		saveFile.getName().replaceAll(".csv", "");
 		
-		String head = "제목, 내용";
-		String firstValue = "베이비 드라이버, 재밌다";
-		String secondValue = "청년경찰, 유쾌하다";
+		String csvFilePath = saveFile.getPath() + ".csv";
+		
+		/*if(!saveFile.exists()){
+			try {
+				saveFile.createNewFile();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}*/
+			
+		
+		int length = resultDatas.getData().length;
+		
+		String rowData[] = new String[length];
+		
+		for(int i=0; i<length; i++){
+			
+			try{
+				ResultDataModel modelData = resultDatas.getData()[i];
+				
+				if(modelData == null){
+					length = i;
+					break;
+				}
+					
+				
+				rowData[i] = new String(modelData.getWidth() + "," 
+						+ modelData.getHeight() + "," 
+						+ modelData.getTime() + "," 
+						+ modelData.getBeetleCount() + "," 
+						+ modelData.getDalay());
+				
+			}catch (Exception e){
+				//ignore
+			}
+		}
+		
+		String head = "가로 크기,세로 크기,소요 시간,딱정 벌레 수,딜레이";
+		
 		
 		BufferedWriter writer;
 		
 		try {
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csvFilePath), "UTF-8"));
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csvFilePath), "MS949"));
 			
 			writer.write(head);
-			writer.write(firstValue);
-			writer.write(secondValue);
+			writer.newLine();
+			
+			for(int i=0; i<length; i++){
+				System.out.println(rowData[i]);
+				writer.write(rowData[i]);
+				writer.newLine();
+			}
 			
 			writer.close();
 			
